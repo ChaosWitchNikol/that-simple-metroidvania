7ae5096e4ad10da86a2846f9c064e203dd678690
NikkiC <cupi.niki@gmail.com>
2019-07-04 22:34:06 +0200

    first commit

---
+++ source/scripts/_constants.gd
@@ -0,0 +1,15 @@
+extends Object
+class_name C
+
+const TILE_SIZE : int = 32
+const TILE_SIZEF : float = TILE_SIZE * 1.0
+const HALF_TILE_SIZE : int = TILE_SIZE / 2
+const HALF_TILE_SIZEF : float = TILE_SIZEF / 2.0
+
+const GRAVITY_VALUE : float = 9.8
+const GRAVITY_VECTOR : Vector2 = Vector2(0, 1)
+
+const FLOOR_VECTOR : Vector2 = Vector2(0, -1)
+
+const SNAP_VECTOR : Vector2 = Vector2(0, 24)
+const JUMP_SNAP_VECTOR : Vector2 = Vector2(0, 0)
---
+++ source/scripts/hero.gd
@@ -0,0 +1,60 @@
+extends KinematicBody2D
+
+export(float, 0, 39.2) var gravity : float = C.GRAVITY_VALUE
+export(float, 0, 400) var mass : float = 100
+export(float, 0, 1000) var movement_speed : float = 500
+export(float, 0, 2000) var jump_force : float = 15
+export(int, 0, 10) var allowed_jumps : int = 1
+export(float, 0, 1) var jump_delay : float = 0.1
+export(bool) var allow_wall_climbing : bool = false
+
+
+var linear_velocity := Vector2()
+var jumps_count : int = 0
+var jump_timeout : float = 0
+
+
+
+func _process(delta: float) -> void:
+	if linear_velocity.x != 0:
+		$Sprite.flip_h = linear_velocity.x < 0
+	
+	#	decrease delay between jumps
+	if jump_timeout > 0:
+		jump_timeout -= delta
+
+func _physics_process(delta: float) -> void:
+	#	process gravity
+	linear_velocity.y += gravity * mass * delta
+	
+	if is_on_floor():
+		linear_velocity.y = 0
+		jumps_count = 0
+	
+	if is_on_wall() and allow_wall_climbing:
+		jumps_count = 0
+		if linear_velocity.y > 0:
+			linear_velocity.y *= 0.75
+	
+	var snap_vector := C.SNAP_VECTOR
+	
+	if Input.is_action_just_pressed("ui_select"):
+		if jump_timeout <= 0 and jumps_count < allowed_jumps:
+			linear_velocity.y = -jump_force * C.TILE_SIZEF
+			jumps_count += 1
+			jump_timeout = jump_delay
+			snap_vector = C.JUMP_SNAP_VECTOR
+			
+	
+	linear_velocity.x = 0
+	if Input.is_action_pressed("ui_left"):
+		linear_velocity.x = -movement_speed * delta * C.TILE_SIZEF
+	if Input.is_action_pressed("ui_right"):
+		linear_velocity.x = movement_speed * delta * C.TILE_SIZEF
+	
+	#	when on ceiling imeddiatelly apply gravity
+	if is_on_ceiling():
+		linear_velocity.y += gravity
+	
+	#	finally move
+	linear_velocity = move_and_slide_with_snap(linear_velocity, snap_vector, C.FLOOR_VECTOR)
