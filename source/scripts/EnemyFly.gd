f6ff59c9525e349c0b7fad42f424dfa4b246cc5c
NikkiC <cupi.niki@gmail.com>
2019-07-14 09:26:05 +0200

    enemy fly with enemy spawner

--- source/scripts/EnemyFly.gd
+++ source/scripts/EnemyFly.gd
@@ -1,7 +1,10 @@
 extends EnemyBase
-class_name EnemyStatic
+class_name EnemyFly
 
 #==== custom processors ====
 #= @override =
 func process_gravity(delta : float) -> void:
+	pass
+#= @override =
+func process_movement(delta : float) -> void:
 	pass
