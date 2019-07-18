1a9a146ffdf04df2080aa31aaac9cc83beb32df6
Nikki C <cupi.niki@gmail.com>
2019-07-18 20:56:58 +0200

    working pathway preview

---
+++ source/scripts/PathwayPoint.gd
@@ -0,0 +1,14 @@
+extends Position2D
+class_name PathwayPoint
+
+
+#==== export variables ====
+export(float, -1, 2048, 0.25) var wait_time : float = -1 setget _set_wait_time
+
+
+#==== setters ====
+func _set_wait_time(value : float = -1) -> void:
+	if value < 0.01:
+		value = -1
+	wait_time = value
+
