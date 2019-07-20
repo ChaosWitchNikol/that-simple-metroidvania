a2a74feb0a398a01d9a8640a4f07244cf8383515
Nikki C <cupi.niki@gmail.com>
2019-07-20 15:08:02 +0200

    basic flying enemy

--- source/scripts/EnemyFly.gd
+++ source/scripts/EnemyFly.gd
@@ -1,10 +1,16 @@
 extends EnemyBase
 class_name EnemyFly
 
+
+	
+
 #==== custom processors ====
 #= @override =
 func process_gravity(delta : float) -> void:
 	pass
 #= @override =
 func process_movement(delta : float) -> void:
+	if target:
+		var direction : Vector2 = (target.position - global_position).normalized()
+		linear_velocity = direction * delta * movement_speed * C.TILE_SIZE
 	pass
