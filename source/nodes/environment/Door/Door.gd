15d2c3cc200b680f0bdde7aaf987626943f4a421
Nikki C <cupi.niki@gmail.com>
2019-09-13 16:00:42 +0200

    doors reloack / unlock code

--- source/nodes/environment/Door/Door.gd
+++ source/nodes/environment/Door/Door.gd
@@ -5,9 +5,23 @@ class_name Door
 export(C.FACING) var facing : int = C.FACING.RIGHT setget _set_facing
 export(Color) var color_front : Color = Color("#FFFFFF") setget _set_color_front
 export(Color) var color_back : Color = Color("#FFFFFF") setget _set_color_back
+export(float, 0.01, 4096, 0.01) var lock_timeout : float = 0.5 setget _set_lock_timeout
 
 
 
+
+
+#==== functions ====
+func unlock() -> void:
+	$Front/Collision.disabled = true
+	$Lock/Collision.disabled = true
+	self_modulate.a = 0
+
+
+func relock() -> void:
+	$Front/Collision.disabled = true
+	$Lock/Collision.disabled = true
+	self_modulate.a = 1
 
 
 
@@ -22,6 +36,7 @@ func _set_facing(value : int) -> void:
 		direction = 1
 	$Back.position.x = abs($Back.position.x) * direction
 	$Back/Body.position.x = abs($Back/Body.position.x) * direction
+	$Lock.position.x = abs($Lock.position.x) * direction
 
 
 func _set_color_front(value : Color) -> void:
@@ -38,7 +53,19 @@ func _set_color_back(value : Color) -> void:
 	$Back.self_modulate = value
 
 
+func _set_lock_timeout(value : float) -> void:
+	lock_timeout = value
+
+
 
 #==== signals ====
 func _on_Lock_area_entered(area: Area2D) -> void:
-	print("player action entered")
+	call_deferred("unlock")
+
+
+func _on_Lock_body_exited(body: PhysicsBody2D) -> void:
+	$LockTimeout.call_deferred("start", lock_timeout)
+
+
+func _on_LockTimeout_timeout() -> void:
+	call_deferred("relock")
