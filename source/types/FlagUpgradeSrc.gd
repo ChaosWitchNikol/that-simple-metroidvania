d9b041634aabbc56e1517d7a052e637e49e02729
Nikki C <cupi.niki@gmail.com>
2019-10-25 20:25:16 +0200

    flat upgrade src variable name added

--- source/types/FlagUpgradeSrc.gd
+++ source/types/FlagUpgradeSrc.gd
@@ -4,6 +4,7 @@ class_name FlagUpgradeSrc
 
 
 export(String) var unique_name : String
+export(String) var variable_name : String
 export(String) var flag_name : String
 export(int, 0, 8192) var size : int = 64
 
