f68ed3c8703716a6767d7226bd0c175e729c49d4
Nikki C <cupi.niki@gmail.com>
2019-11-11 16:03:32 +0100

    boss variables #1

--- source/nodes/Boss/Boss.gd
+++ source/nodes/Boss/Boss.gd
@@ -2,11 +2,29 @@ tool
 extends KinematicBody2D
 class_name Boss
 
-
+#==== variables ====
+#==== life ====
 export(Array, Resource) var phases : Array = [] setget _set_phases
+var target : Node setget set_target
+var target_in_range : bool = false
+#==== gravity ====
+export(float) var gravity_value : float = C.GRAVITY_VALUE
+export(float) var mass : float = 100
+var gravity_vector : Vector2 = C.GRAVITY_VECTOR setget set_gravity_vector
+#==== movement ====
+export(float) var movement_speed : float = 300
+export(C.FACING) var facing : int = C.FACING.RIGHT setget set_facing
+var linear_velocity : Vector2 = Vector2()
+var forward_vector : Vector2 = Vector2()
+var snap_vector : Vector2 = C.SNAP_VECTOR
+var floor_vector : Vector2 = C.FLOOR_VECTOR
 
 
-
+func get_current_phase() -> BossPhaseSrc:
+	if phases.empty():
+		return null
+	return null
+	
 
 
 #==== setters ====
@@ -17,7 +35,25 @@ func _set_phases(array : Array) -> void:
 				array[i] = BossPhaseSrc.new()
 				
 	phases = array
-	
+
+
+func set_target(value : Node2D) -> void:
+	target = value
+
+
+func set_gravity_vector(value : Vector2) -> void:
+	gravity_vector = value.normalized()
+	snap_vector = gravity_vector * C.SNAP_VECTOR.length()
+	floor_vector = -gravity_vector
+	forward_vector = U.gravity_vector2forward_vector(gravity_vector, facing)
+
+
+func set_facing(value : int) -> void:
+	facing = value
+	$Sprite.flip_h = facing == C.FACING.LEFT
+	$View.position.x = abs($View.position.x) * facing
+	$AttackRange.position.x = abs($AttackRange.position.x) * facing
+	forward_vector = U.gravity_vector2forward_vector(gravity_vector, facing)
 
 
 
