eca4d73fafdb26dac93930e2d271ae0551eb7aad
Nikki C <cupi.niki@gmail.com>
2019-11-11 15:46:06 +0100

    moved magnitude variable

--- source/types/BossAttackPatternSrc.gd
+++ source/types/BossAttackPatternSrc.gd
@@ -2,9 +2,10 @@ extends Resource
 class_name BossAttackPatternSrc
 
 
-export(int, 1, 1024) var magnitude : int = 1
 export(PackedScene) var attack_scene : PackedScene
 export(float, 0.01, 1000, 0.01) var cooldown : float = 0.01
+export(int, 1, 1024) var magnitude : int = 1
+
 
 
 #==== getters ====
--- source/types/BossPhaseSrc.gd
+++ source/types/BossPhaseSrc.gd
@@ -14,13 +14,24 @@ export(Script) var phase_script : Script
 func get_class() -> String:
 	return C.ClassNames.BossPhaseSrc
 
+func get_magnitude_sum() -> int:
+	if attack_patterns.empty():
+		return 1
+	
+	var magnitude : int = 0
+	
+	for item in attack_patterns:
+		magnitude += item.magnitude
+	
+	return magnitude
+
 
 
 #==== setters ====
 func _set_attack_patterns(array : Array) -> void:
 	if U.in_editor() and not array.empty():
 		for i in range(0, array.size()):
-			if not array[i]:
+			if not array[i] or array[i].get_class() != C.ClassNames.BossAttackPatternSrc:
 				array[i] = BossAttackPatternSrc.new()
 	
 	attack_patterns = array
