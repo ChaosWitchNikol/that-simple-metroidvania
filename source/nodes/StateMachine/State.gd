96a8afcbe6491d50547c485f7b1b894f170f57a0
Nikki C <cupi.niki@gmail.com>
2019-12-05 17:52:49 +0100

    state machine base

---
+++ source/nodes/StateMachine/State.gd
@@ -0,0 +1,15 @@
+extends Node
+class_name StMState
+
+
+
+func _enter_state() -> void:
+	pass
+
+
+func _process_state(host : Node, delta : float) -> void:
+	pass
+
+
+func _exit_state() -> void:
+	pass
