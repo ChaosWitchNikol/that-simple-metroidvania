f6f8f9884a47cb4e8a4751b14f976e30b8bf544e
Nikki C <cupi.niki@gmail.com>
2019-12-08 09:54:28 +0100

    setting new state

--- source/nodes/StateMachine/StM.gd
+++ source/nodes/StateMachine/StM.gd
@@ -3,28 +3,43 @@ class_name StM
 
 signal state_changed
 
+export(NodePath) var host_path : NodePath
 var host : Node
-
 export(NodePath) var initial_state_path : NodePath setget _set_initial_state_path
-var _state : StMState
+var state : StMState
 
 
 #==== node functions ====
 func _ready() -> void:
-	host = get_parent()
+	host = get_node(host_path)
+	set_new_state_from_path(initial_state_path)
 
 
 func _physics_process(delta: float) -> void:
-	if _state:
-		_state.process_state(self, delta)
-		
+	if state:
+		state.process_state(host, delta)
+		var new_state_path : NodePath = state._process_transitions(host, delta)
+		if not new_state_path.is_empty():
+			call_deferred("set_new_state_from_path", new_state_path)
 
 
 #==== setters ====
 func _set_initial_state_path(state: NodePath) -> void:
 	initial_state_path = state
-	if U.in_editor():
-		_state = get_node(state)
+
+
+func _set_state(new_state: StMState) -> void:
+	state = new_state
+	emit_signal("state_changed", state)
+
+
+func set_new_state_from_path(new_state_path : NodePath) -> void:
+	var new_state := get_node(new_state_path)
+	
+	if new_state.get_class() != C.ClassNames.StMState:
+		return
+	
+	_set_state(new_state)
 
 
 
