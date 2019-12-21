00336ab1ecd0d982b1ce19394c8a96b3819fbd5a
cupiniki <cupi.niki@gmail.com>
2019-12-21 15:14:00 +0100

    boss test figt movement

--- source/nodes/Boss/BossPhase.gd
+++ source/nodes/Boss/BossPhase.gd
@@ -1,14 +1,9 @@
-tool
 extends StM
 class_name BossPhase
 
 #==== exports ====
 export(float, 0, 1) var active_at_health_percent : float = 0
 
-#==== node functions ====
-func _enter_tree() -> void:
-	if not get_scene_instance_load_placeholder():
-		set_scene_instance_load_placeholder(true)
 
 #==== getters ====
 func get_class() -> String:
--- source/nodes/Boss/BossPhaseList.gd
+++ source/nodes/Boss/BossPhaseList.gd
@@ -3,27 +3,23 @@ class_name BossPhaseList
 
 
 #==== variables ====
+export(NodePath) var host_path : NodePath
 export(NodePath) var initial_phase_path : NodePath
 var phase : BossPhase
 
 
 #==== node functions ====
 func _ready() -> void:
-	instance_phase_from_path(initial_phase_path)
+	select_current_phase_from_path(initial_phase_path)
 	pass
 
 
 #==== functions ====
-func instance_phase_from_path(phase_path : NodePath) -> void:
-	var new_phase_placeholder := get_node(phase_path)
-	if new_phase_placeholder and new_phase_placeholder is InstancePlaceholder:
-		var new_phase := (new_phase_placeholder as InstancePlaceholder).create_instance()
-		if new_phase.get_class() == C.ClassNames.BossPhase:
-			if phase:
-				var old_phase : BossPhase = phase
-				remove_child(old_phase)
-			phase = new_phase
-
+func select_current_phase_from_path(phase_path : NodePath) -> void:
+	var new_phase : BossPhase = get_node(phase_path)
+	if new_phase.get_class() == C.ClassNames.BossPhase:
+		var old_phase : BossPhase = phase
+		phase = new_phase
 
 
 #==== gettters ====
